2e60ef3a179c602de48aa470104a03f0a5d5b8da
Morphic <osorezetsumei@googlemail.com>
2020-09-23 20:11:21 +0100

    Added Listener for sleep event Added methods to handle what directories are worlds Added single method for all syncing of worlds Added Sleep command for testing (disabled atm)

---
+++ src/main/java/me/morphicdream/worldtimesync/WorldListener.java
@@ -0,0 +1,14 @@
+package me.morphicdream.worldtimesync;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+
+public class WorldListener implements Listener {
+
+    @EventHandler
+    public void onSleep(PlayerBedEnterEvent event) {
+        WorldTimeSync.getInstance().syncWorlds(event.getPlayer().getWorld(), event.getPlayer());
+    }
+
+}
---
+++ src/main/java/me/morphicdream/worldtimesync/WorldTimeSync.java
@@ -0,0 +1,100 @@
+package me.morphicdream.worldtimesync;
+
+import me.morphicdream.worldtimesync.commands.SleepCommand;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class WorldTimeSync extends JavaPlugin {
+
+    private static List<String> worldNames = new ArrayList<String>();
+    private List excluded = Arrays.asList("plugins", "logs", "crash-reports");
+    private static WorldTimeSync instance;
+    private int time = 1000;//This should be 7am mc time
+    private boolean isNight;
+
+    public WorldTimeSync() {
+        instance = this;
+    }
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new WorldListener(), getInstance());
+        //getInstance().getCommand("sleep").setExecutor(new SleepCommand());
+        FilenameFilter filter = new FilenameFilter() {
+            public boolean accept(File pathname, String name) {
+                String lowercase = name.toLowerCase();
+                if (lowercase.endsWith(".dat")) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        };
+        File path = Bukkit.getServer().getWorldContainer();
+        File[] files = path.listFiles();
+        if (files == null) {
+            System.out.println("No worlds found!");
+        } else {
+            for (File file : files) {
+                if (file.isDirectory() && file.list(filter) != null) {
+                    //for (String string : file.list(filter)) {
+                    List list = Arrays.asList(file.list(filter));
+                    if(list.contains("level.dat")) {
+                        System.out.println(file.getName());
+                        worldNames.add(file.getName());
+                        // }
+                    }
+                }
+            }
+        }
+    }
+
+    public List<String> getWorldNames() {
+        return worldNames;
+    }
+
+    public static WorldTimeSync getInstance() {
+        return instance;
+    }
+
+    public void syncWorlds(World world, final Player sleeper) {
+        System.out.println(sleeper.getName() + " called syncWorlds");
+        if (world.getTime() > 13000 && world.getTime() < 23460) {
+            isNight = true;
+        }
+        if (isNight) {
+            new BukkitRunnable() {
+                public void run() {
+                    for (String string : getWorldNames()) {
+                        World w = Bukkit.getWorld(string);
+                        if(w.isThundering()) {
+                            w.setThundering(false);
+                        }
+                        if(w.hasStorm()) {
+                            w.setStorm(false);
+                        }
+                        w.setTime(time);
+                        for (Player p : w.getPlayers()) {
+                            if (w == p.getWorld()) {
+                                p.sendMessage(sleeper.getDisplayName() + " has slept and moved time forward!");
+                            } else {
+                                p.sendMessage(sleeper.getDisplayName() + " has slept in another world and moved time forward");
+                            }
+                            System.out.println(w.getName() + " has had its time set to " + time);
+                        }
+                    }
+                }
+            }.runTaskLater(WorldTimeSync.getInstance(), 20 * 5);
+        }
+    }
+}
